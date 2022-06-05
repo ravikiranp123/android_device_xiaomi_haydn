4bd8e4ca424306759b86639462589cbd0e33ac91
Ravi Kiran P <ravikirancg123@gmail.com>
2022-06-05 20:20:57 +0530

    Ignore sepolicy neverallow

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,6 +193,7 @@ VENDOR_SECURITY_PATCH := 2022-02-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
